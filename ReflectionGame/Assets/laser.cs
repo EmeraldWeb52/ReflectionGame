fe18327aa01dd92debcd0d66f39c1fde75521d03
JALBOD <benetone8@gmail.com>
2025-03-30 10:48:21 +0300

    Update laser.cs

--- ReflectionGame/Assets/laser.cs
+++ ReflectionGame/Assets/laser.cs
@@ -39,7 +39,7 @@ public class Laser : MonoBehaviour
         points.Clear();
         points.Add(start);
 
-        int UltraMegaNecess;
+        int UltraMegaNecess = 0;
         //makes sure it only reflect <100 timse
         for (int i = 0; i < maxReflections; i++)
         {
