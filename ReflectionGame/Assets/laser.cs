e1c51afd2e7f8164c16255670e4ea932a8accd42
JALBOD <benetone8@gmail.com>
2025-03-30 10:47:13 +0300

    Debug

--- ReflectionGame/Assets/laser.cs
+++ ReflectionGame/Assets/laser.cs
@@ -39,7 +39,7 @@ public class Laser : MonoBehaviour
         points.Clear();
         points.Add(start);
 
-
+        int UltraMegaNecess;
         //makes sure it only reflect <100 timse
         for (int i = 0; i < maxReflections; i++)
         {
@@ -53,6 +53,7 @@ public class Laser : MonoBehaviour
                 {
                     dir = Vector2.Reflect(dir, hit.normal);
                     start = hit.point + dir * 0.05f;
+                    UltraMegaNecess++;
                     continue;
                 }
 
@@ -60,10 +61,12 @@ public class Laser : MonoBehaviour
                 if (LaserDetectorTag != "" && hit.collider.gameObject.tag == LaserDetectorTag && hit.collider.gameObject.GetComponent<LightSensor>())
                 {
                      hit.collider.gameObject.GetComponent<LightSensor>().SetTurnedOn();
+                     UltraMegaNecess++;
                      break;
                 }
                 else
                 {
+                     UltraMegaNecess++;
                      break;
                 }
             }
@@ -75,6 +78,7 @@ public class Laser : MonoBehaviour
                 break;
             }
         }
+        Debug.Log("Loop does " + UltraMegaNecess + " times lasering");
         //renders the line
         lineRen.positionCount = points.Count;
         lineRen.SetPositions(points.ToArray());
