728e975d59fe13a14a989302ded22c299876972f
EmeraldWeb52 <brooksweb1@gmail.com>
2025-04-02 15:15:37 -0500

    rotatory

--- ReflectionGame/Assets/Scripts/Button.cs
+++ ReflectionGame/Assets/Scripts/Button.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using UnityEngine.Events;
 public class Button : MonoBehaviour
 {
-    UnityEvent UE;
+    [SerializeField]UnityEvent UE;
     void OnCollisionEnter2D(Collision2D coll)
     {
          if (coll.gameObject.GetComponent<Rigidbody2D>()) UE.Invoke();
--- ReflectionGame/Assets/Scripts/MirrorRotatin.cs
+++ ReflectionGame/Assets/Scripts/MirrorRotatin.cs
@@ -1,5 +1,26 @@
+using System.Collections;
 using UnityEngine;
+
 public class MirrorRotatin : MonoBehaviour
 {
-    public void RotateCertAmount(float amount) => this.transform.Rotate(0,0,amount);
+    public void RotateCertAmount(float amount, float speed)
+    {
+        StartCoroutine(RotateOverTime(amount, speed));
+    }
+
+    IEnumerator RotateOverTime(float amount, float speed)
+    {
+        float rotated = 0;
+        float direction = Mathf.Sign(amount); 
+        while (Mathf.Abs(rotated) < Mathf.Abs(amount))
+        {
+            float rotationStep = direction * speed * Time.deltaTime;
+            if (Mathf.Abs(rotated + rotationStep) > Mathf.Abs(amount))
+                rotationStep = amount - rotated; 
+
+            transform.Rotate(0, 0, rotationStep);
+            rotated += rotationStep;
+            yield return null;
+        }
+    }
 }
